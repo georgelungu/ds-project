937c076110f7a3e646b373efd1251cf22ee4c6fc
George Lungu <george.lungu1896@gmail.com>
2023-10-15 14:19:03 +0300

    added some navbars

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app').innerHTML += `
     <div>
       <p>Digital Stack Project</p>
       <p>By George Lungu</p>
